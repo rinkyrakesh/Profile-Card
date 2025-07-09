697acc33b9e68f024b6c3f2ee452a4e9915f26d4
Rinky Singh <rinsingh@publicisgroupe.net>
2025-07-10 01:32:52 +0530

    first update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const skills = [
+  {
+    skill: "HTML CSS",
+    level: "beginner",
+    color: "red",
+  },
+  {
+    skill: "Javascript",
+    level: "intermediate",
+    color: "yellow",
+  },
+  {
+    skill: "React",
+    level: "advanced",
+    color: "green",
+  },
+  {
+    skill: "HTML CSS",
+    level: "intermediate",
+    color: "yellow",
+  },
+  {
+    skill: "Javascript",
+    level: "beginner",
+    color: "red",
+  },
+  {
+    skill: "React",
+    level: "beginner",
+    color: "red",
+  },
+];
+
+function Avatar() {
+  return <img className="avatar" src="avatar.png" alt="Rinky Singh" />;
+}
+function Intro() {
+  return (
+    <div>
+      <h1>Rinky Singh</h1>
+      <p>
+        Creative and detail-oriented Front-End Developer with a passion for
+        crafting seamless user experiences and clean, maintainable code. Skilled
+        in translating UI/UX designs into responsive, interactive websites and
+        web applications using modern frameworks and technologies.
+      </p>
+    </div>
+  );
+}
+function SkillList() {
+  return (
+    <div className="skill-list">
+      {skills.map((skill) => (
+        <Skill skill={skill.skill} level={skill.level} color={skill.color} />
+      ))}
+    </div>
+  );
+}
+
+function Skill({ skill, level, color }) {
+  return (
+    <div className="skill" style={{ backgroundColor: color }}>
+      <span>{skill}</span>
+      <span>
+        {level === "beginner" && "😊"}
+        {level === "intermediate" && "💪"}
+        {level === "advanced" && "🌟"}
+      </span>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
     </div>
   );
 }
